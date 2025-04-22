1fffdffb54d153e4d7e752830cdc2985c9ef7c89
Iza Dias <izinhagamer16@gamil.com>
2025-04-22 12:07:28 -0300

    primeiro commit

---
+++ src/main/java/poo/sistemabiblioteca/izabellyc/d/berghe/a/info/SistemaBibliotecaIzabellyCDBerghe2AInfo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.sistemabiblioteca.izabellyc.d.berghe.a.info;
+
+/**
+ *
+ * @author 2024105070070
+ */
+public class SistemaBibliotecaIzabellyCDBerghe2AInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
